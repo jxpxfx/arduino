9b4370101bcbc15ad17326c54870a1fae173f52a
jxpxfx <jferreira@latte.vpn.mmsi.com>
2015-11-18 08:12:03 -0800

    Initial Code

---
+++ BlinkRGB/BlinkRGB.ino
@@ -0,0 +1,43 @@
+/*
+ ESP8266 Blink by Simon Peter
+ Blink the blue LED on the ESP-01 module
+ This example code is in the public domain
+ 
+ The blue LED on the ESP-01 module is connected to GPIO1 
+ (which is also the TXD pin; so we cannot use Serial.print() at the same time)
+ 
+ Note that this sketch uses BUILTIN_LED to find the pin with the internal LED
+*/
+
+int r = 12;
+int g = 13;
+int b = 15;
+
+void setup() {
+  pinMode(BUILTIN_LED, OUTPUT);     // Initialize the BUILTIN_LED pin as an output
+  pinMode(r, OUTPUT);
+  pinMode(g, OUTPUT);
+  pinMode(b, OUTPUT);
+}
+
+// the loop function runs over and over again forever
+void loop() {
+  digitalWrite(r, LOW);   // Turn the LED on (Note that LOW is the voltage level
+  delay(500);                      // Wait for a second
+  digitalWrite(r, HIGH);  // Turn the LED off by making the voltage HIGH
+  digitalWrite(g, HIGH);  // Turn the LED off by making the voltage HIGH
+  digitalWrite(b, HIGH);  // Turn the LED off by making the voltage HIGH
+  delay(500);                      // Wait for two seconds (to demonstrate the active low LED)
+  digitalWrite(g, LOW);   // Turn the LED on (Note that LOW is the voltage level
+  delay(500);                      // Wait for a second
+  digitalWrite(r, HIGH);  // Turn the LED off by making the voltage HIGH
+  digitalWrite(g, HIGH);  // Turn the LED off by making the voltage HIGH
+  digitalWrite(b, HIGH);  // Turn the LED off by making the voltage HIGH
+  delay(500);                      // Wait for two seconds (to demonstrate the active low LED)
+  digitalWrite(b, LOW);   // Turn the LED on (Note that LOW is the voltage level
+  delay(500);                      // Wait for a second
+  digitalWrite(r, HIGH);  // Turn the LED off by making the voltage HIGH
+  digitalWrite(g, HIGH);  // Turn the LED off by making the voltage HIGH
+  digitalWrite(b, HIGH);  // Turn the LED off by making the voltage HIGH
+  delay(500);                      // Wait for two seconds (to demonstrate the active low LED)
+}
---
+++ Sonar/Sonar.ino
@@ -0,0 +1,56 @@
+#include <LiquidCrystal.h>
+
+//LiquidCrystal lcd(12, 11, 5, 4, 3, 2);
+
+int pingPin = 3;
+int inPin = 2;
+long duration, inches, cm;
+int indec, cmdec;
+int inchconv = 147;
+int cmconv = 59;
+String s1, s2;
+
+void setup() {
+  Serial.begin(9600);
+  //lcd.begin(16, 2);
+  pinMode(pingPin, OUTPUT);
+  pinMode(inPin, INPUT);
+}
+
+void loop()
+{
+  digitalWrite(pingPin, LOW);
+  delayMicroseconds(2);
+  digitalWrite(pingPin, HIGH);
+  delayMicroseconds(10);
+  digitalWrite(pingPin, LOW);
+
+  duration = pulseIn(inPin, HIGH);
+
+  inches = microsecondsToInches(duration);
+  indec = (duration - inches * inchconv) * 10 / inchconv;
+  cm = microsecondsToCentimeters(duration);
+  cmdec = (duration - cm * cmconv) * 10 / cmconv;
+  s1 = String(inches) + "." + String(indec) + "in" + "     ";
+  s2 = String(cm) + "." + String(cmdec) + "cm" + "     ";
+//  lcd.setCursor(0, 0);
+//  lcd.print(s1);
+//  lcd.setCursor(0,1);
+//  lcd.print(s2);
+  //Send to the cloud
+  //Debug
+  Serial.println(s2);
+
+  delay(500);
+}
+
+long microsecondsToInches(long microseconds)
+{
+  return microseconds / inchconv;
+}
+
+long microsecondsToCentimeters(long microseconds)
+{
+  return microseconds / cmconv;
+}
+
