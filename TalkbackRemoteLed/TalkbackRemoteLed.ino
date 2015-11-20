6ad5b3fcc6e1450ab4ecf3dfdf7c6b1b6718effd
jxpxfx <jxpxfx@users.noreply.github.com>
2015-11-19 21:10:17 -0800

    Create TalkbackRemoteLed.ino

---
+++ TalkbackRemoteLed/TalkbackRemoteLed.ino
@@ -0,0 +1,120 @@
+/*
+  Web client
+
+ This sketch connects to a website (http://www.google.com)
+ using an Arduino Wiznet Ethernet shield.
+
+ Circuit:
+ * Ethernet shield attached to pins 10, 11, 12, 13
+
+ created 18 Dec 2009
+ by David A. Mellis
+ modified 9 Apr 2012
+ by Tom Igoe, based on work by Adrian McEwen
+
+ */
+
+#include <SPI.h>
+#include <Ethernet.h>
+
+// Enter a MAC address for your controller below.
+// Newer Ethernet shields have a MAC address printed on a sticker on the shield
+byte mac[] = { 0xDE, 0xAD, 0xBE, 0xEF, 0xFE, 0xED };
+// if you don't want to use DNS (and reduce your sketch size)
+// use the numeric IP instead of the name for the server:
+//IPAddress server(74,125,232,128);  // numeric IP for Google (no DNS)
+char server[] = "api.thingspeak.com";    // name address for Google (using DNS)
+
+// Set the static IP address to use if the DHCP fails to assign
+IPAddress ip(192, 168, 1, 177);
+
+// Initialize the Ethernet client library
+// with the IP address and port of the server
+// that you want to connect to (port 80 is default for HTTP):
+EthernetClient client;
+String response;
+
+void setup() {
+  pinMode(8, OUTPUT);
+  
+  // Open serial communications and wait for port to open:
+  Serial.begin(9600);
+  while (!Serial) {
+    ; // wait for serial port to connect. Needed for Leonardo only
+  }
+
+  // start the Ethernet connection:
+  if (Ethernet.begin(mac) == 0) {
+    Serial.println("Failed to configure Ethernet using DHCP");
+    // no point in carrying on, so do nothing forevermore:
+    // try to congifure using IP address instead of DHCP:
+    Ethernet.begin(mac, ip);
+  }
+  // give the Ethernet shield a second to initialize:
+  delay(1000);
+  
+  connect();
+}
+
+void loop()
+{
+  if (client.connected()) {
+    Serial.println("client is connected");
+    if (client.available()) {
+      Serial.println("client is available");
+      processResponse();
+      Serial.println("after response");
+      client.stop();
+      Serial.println("after stop");
+    }
+  } else {
+    
+    connect();  
+  }
+  delay(500);
+}
+
+void connect() {
+  Serial.println("connecting...");
+  // if you get a connection, report back via serial:
+  if (client.connect(server, 80)) {
+    Serial.println("connected");
+    // Make a HTTP request:
+    client.println("GET /talkbacks/4103/commands/execute?api_key=MY_THING HTTP/1.1");
+    client.println("Host: www.google.com");
+    client.println("Connection: close");
+    client.println();
+  }
+  else {
+    // if you didn't get a connection to the server:
+    Serial.println("connection failed");
+  }
+}
+
+void processResponse() {
+    Serial.println("process response");
+    if (client.available()) {
+      Serial.println("process is available");
+    }
+    while (client.available()) {
+    char c = client.read();
+//    Serial.print("->");
+    response+=c;
+    if (response.length()==653)
+    {
+      String cmdon = response.substring(650,652);
+      String cmdoff = response.substring(650,653);
+      Serial.print("--->");
+      if (cmdon == "ON") {        
+        Serial.println(cmdon);
+        digitalWrite(8, HIGH);        
+      } else if (cmdoff == "OFF") {
+        Serial.println(cmdoff);
+        digitalWrite(8, LOW);
+      } else {
+        Serial.println("NO COMMAND FOUND");
+      }
+    }    
+  }
+  response = "";
+}
